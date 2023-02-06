b9d55354c6c649ba90a3338c5d8dfdd5d038d3d7
PavelGL72admdocker <fire_ra@bk.ru>
2023-02-06 23:02:11 +0300

    First commit

---
+++ app/src/main/java/com/vk/directop/phonebookone/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.vk.directop.phonebookone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vk.directop.phonebookone.ui.theme.PhoneBookOneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PhoneBookOneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PhoneBookOneTheme {
+        Greeting("Android")
+    }
+}
