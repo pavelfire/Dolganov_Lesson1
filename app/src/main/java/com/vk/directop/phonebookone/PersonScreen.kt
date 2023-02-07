d6355fce7df513ae21da29cef12050e113e9331a
PavelGL72admdocker <fire_ra@bk.ru>
2023-02-07 11:02:06 +0300

    onClick toast added

--- app/src/main/java/com/vk/directop/phonebookone/PersonScreen.kt
+++ app/src/main/java/com/vk/directop/phonebookone/PersonScreen.kt
@@ -1,11 +1,6 @@
 package com.vk.directop.phonebookone
 
-import android.Manifest
-import android.Manifest.permission.CALL_PHONE
 import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -18,10 +13,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat
-import androidx.core.app.ActivityCompat.requestPermissions
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.startActivity
 
 
 @Composable
@@ -92,7 +83,7 @@ fun PersonList(
                     .fillMaxWidth()
                     .padding(8.dp)
                     .clickable {
-                        Toast.makeText(context, "Name is ${currentName.name}", Toast.LENGTH_LONG).show()
+                        Toast.makeText(context, "Name is ${currentName.name}", Toast.LENGTH_SHORT).show()
                     }
             )
             Text(
